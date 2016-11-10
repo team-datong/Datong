fdf6945bc8140c3532ea825bec5b618bc57f94c8
Lizzy Steger <lizzy.steger@gmail.com>
2016-11-10 06:26:08 +0000

    added more tests and deleted unnecessary step definitions.

--- features/step_definitions/resource_upload_steps.rb
+++ features/step_definitions/resource_upload_steps.rb
@@ -10,14 +10,6 @@ module WithinHelpers
 end
 World(WithinHelpers)
 
-Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-
-When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-  fill_in(field, :with => value)
-end
-
 And /^(?:|I )upload the "(.*)"$/ do |filename|
     pending
 end
@@ -29,30 +21,3 @@ end
 Then /^(?:|I )should see the "(.*)" button$/ do |button|
     pending
 end
-
-When /^(?:|I )follow (.*)$/ do |link|
-  click_link(link)
-end
-
-Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
-  regexp = Regexp.new(regexp)
-
-  if page.respond_to? :should
-    page.should have_xpath('//*', :text => regexp)
-  else
-    assert page.has_xpath?('//*', :text => regexp)
-  end
-end
-
-And /^I should not see (.*)$/ do |text|
-    expect(page).not_to have_content(text)
-end
-
-When /^(?:|I )press "([^"]*)"$/ do |button|
-  click_button(button)
-end
-
-Given /^I am not logged in$/ do
-  pending
-end
-
