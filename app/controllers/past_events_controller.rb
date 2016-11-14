00ec08ac24ad2af3e9d2d74a0eb9b83271301094
Jeremy Park <jeremyjpark@gmail.com>
2016-11-13 22:49:47 -0800

    jp - add skeleton and spec tests for PastEvents

---
+++ app/controllers/past_events_controller.rb
@@ -0,0 +1,25 @@
+class PastEventsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ spec/past_events_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe '.index' do
+
+end
+
+describe '.new'
+
+describe '.create'
+
+describe '.edit'
+
+describe '.update'
+
+describe '.destroy'
