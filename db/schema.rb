94cc3a2b0e0abc2c10ac46be680b696c36523fed
Aditi Ganpule <aditiganpule@Aditis-MacBook-Pro-2.local>
2016-11-26 11:26:46 -0800

    ag-Set up mailer for devise, storing user names on registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116043143) do
+ActiveRecord::Schema.define(version: 20161126192124) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161116043143) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
