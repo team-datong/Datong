962200ce559961edf19f2176761250f2f925cef4
Jeremy Park <jeremyjpark@gmail.com>
2016-11-28 13:17:22 -0800

    jp - modified default devise controller with additional params

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  validates :fname, presence: true
+  validates :lname, presence: true
+
 end
