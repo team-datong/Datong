5f8e2cb8ee52dea62abfefa198b483f2b4091f18
susannasouv <ssouv@berkeley.edu>
2016-12-01 15:52:37 -0800

    ss - Define step def for not being logged in as a certain level

--- features/step_definitions/user_permissions_steps.rb
+++ features/step_definitions/user_permissions_steps.rb
@@ -17,5 +17,19 @@ Given /^I am a(?:n?) (.*)$/ do |level|
 end
 
 Given /^I am not a(?:n?) (.*)$/ do |level|
-  pending
+  email = 'datongtest@datong.berkeley.edu'
+  password = 'hunter2'
+  User.create(:email => email,
+              :password => password,
+              :fname => 'first',
+              :lname => 'last',
+              :confirmed_at => Time.now).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Log in"
+
+  role = level.downcase.split(" ")
+  User.last.update_attribute("is_#{role.join('_')}", false)
 end
