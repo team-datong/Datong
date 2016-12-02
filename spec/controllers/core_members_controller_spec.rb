be11f3617a892b102cb57317b797eaf01286c14d
Jeremy Park <jeremyjpark@gmail.com>
2016-12-01 16:21:03 -0800

    jp - increase coverage for core members controller

--- spec/controllers/core_members_controller_spec.rb
+++ spec/controllers/core_members_controller_spec.rb
@@ -1,6 +1,58 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe CoreMembersController, type: :controller do
+describe CoreMembersController, type: :controller do
+  render_views
+
+  before :all do
+    @normal_user = FactoryGirl.create(
+        :user,
+        email: 'normal@user.com',
+        fname: 'normal',
+        lname: 'user',
+        confirmed_at: Time.now,
+        is_core_member: false,
+        is_event_admin: false,
+        is_account_admin: false
+    )
+
+    @core = FactoryGirl.create(
+        :user,
+        email: 'core@member.com',
+        fname: 'core',
+        lname: 'member',
+        confirmed_at: Time.now,
+        is_core_member: true,
+        is_event_admin: false,
+        is_account_admin: false
+    )
+
+    @event_admin = FactoryGirl.create(
+        :user,
+        email: 'event@admin.com',
+        fname: 'event',
+        lname: 'admin',
+        confirmed_at: Time.now,
+        is_core_member: false,
+        is_event_admin: true,
+        is_account_admin: false
+    )
+
+    @account_admin = FactoryGirl.create(
+        :user,
+        email: 'account@admin.com',
+        fname: 'account',
+        lname: 'admin',
+        confirmed_at: Time.now,
+        is_core_member: false,
+        is_event_admin: false,
+        is_account_admin: true
+    )
+  end
+
+  before :each do
+    @controller = CoreMembersController.new
+  end
 
   describe "GET #index" do
     it "returns http success" do
@@ -11,21 +63,32 @@ RSpec.describe CoreMembersController, type: :controller do
 
   describe "GET #edit_multiple" do
     it "returns http success" do
+      sign_in(@account_admin)
+
       get :edit_multiple
       expect(response).to have_http_status(:success)
     end
     it "lists all users" do
+      sign_in(@account_admin)
+
       get :edit_multiple
-      expect(assigns(:users)).to eq(User.all)
+      expect(assigns(:users).sort).to eq(User.all)
     end
+
+    it "redirects if no permission" do
+      get :edit_multiple
+      expect(response).to have_http_status(:redirect)
+    end
+
   end
 
   describe "PUT #update_multiple" do
     before :each do
-      @user1 = User.create(email: 'user1@datong.com', password: 'password')
-      @user2 = User.create(email: 'user2@datong.com', password: 'passwordpassword')
+      @user1 = User.create(email: 'user1@datong.com', password: 'password', fname: 'user1', lname: 'test')
+      @user2 = User.create(email: 'user2@datong.com', password: 'passwordpassword', fname: 'user2', lname: 'test')
     end
     it "assigns core_members" do
+      sign_in(@account_admin)
       put :update_multiple, :user_ids => [@user1.id]
       assigns(:core_members).should eq(["#{@user1.id}"])
       @user1.reload
@@ -34,6 +97,8 @@ RSpec.describe CoreMembersController, type: :controller do
       @user2.is_core_member.should be_falsey
     end
     it "correctly updates one user" do
+      sign_in(@account_admin)
+
       put :update_multiple, :user_ids => [@user1.id]
       @user1.reload
       @user1.is_core_member.should be_truthy
@@ -41,6 +106,8 @@ RSpec.describe CoreMembersController, type: :controller do
       @user2.is_core_member.should be_falsey
     end
     it "correctly updates unchecks" do
+      sign_in(@account_admin)
+
       put :update_multiple, :user_ids => [@user1.id]
       @user1.reload
       @user1.is_core_member.should be_truthy
@@ -53,6 +120,8 @@ RSpec.describe CoreMembersController, type: :controller do
       @user2.is_core_member.should be_falsey
     end
     it "redirects to index" do
+      sign_in(@account_admin)
+
       put :update_multiple, :user_ids => [@user2.id]
       expect(response).to redirect_to(core_members_index_path)
     end
